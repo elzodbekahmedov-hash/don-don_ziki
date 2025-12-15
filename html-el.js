2a48556c66d131bedac01ca5f42836e19d89bcdc
elzodbekahmedov <elzodbekahmedov@gmail.com>
2025-12-15 20:03:56 +0500

    first comit

---
+++ html-el.js
@@ -0,0 +1,9 @@
+const elHands = document.querySelectorAll('.hand-js');
+const elUser = document.querySelector('#user');
+const elRobot = document.querySelector('#robot');
+const elRefershGame = document.querySelector('#refreshGame');
+const elGameZone = document.querySelector('#gameZone');
+const elResultZone = document.querySelector('#resultZone');
+
+
+export { elHands, elUser, elRobot, elRefershGame, elGameZone, elResultZone };
